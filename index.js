6990adb54fda126883cf270202b46bf46d60c3c2
Vicente <vcastello@antel.net.uy>
2019-11-27 17:45:08 -0300

    se agrega resize de la imagen

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
+const sharp = require('sharp')
 console.log("Hola tensor!")
 
-function readImage(path) {
+async function readImage(path) {
     const imageBuffer = fs.readFileSync(path);
-    const tfimage = tf.node.decodeImage(imageBuffer);
+    imageBufferResized = await sharp(imageBuffer).resize(100,100).toBuffer()
+    const tfimage = tf.node.decodeImage(imageBufferResized);
     return tfimage;
 }
 
 async function detect() {
     let labels = ['Vicente', 'Sebastian', 'Paulo'];
     let model = await tf.loadLayersModel('file://model/model.json');
-    model.summary();
-    tfimage = readImage('images/vicente2.jpg');
+   // model.summary();
+    tfimage = await readImage('images/grande.png');
+    console.log(tfimage);
     tfimage = tfimage.div(255.0)
-    tfimage.print();
+   // tfimage.print();
     newtensor = tfimage.expandDims()
     result = model.predict(newtensor);
     result.print()
